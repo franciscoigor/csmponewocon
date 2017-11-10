ccfe7aa4b661d2b8a700946d96f30305ebd6ff2f
Francisco Aviles <francisco.aviles@flex.com>
2017-11-10 10:54:34 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -5,6 +5,10 @@ function app_login($scope, app, $q) {
     if (!$scope.data) {
         $scope.data = {};
     }
+    var algo = function(){
+        debugger;
+      app.call('blank.test',{name:'Jona'})  
+    };
     var checkSupport = function () {
         var deferred = $q.defer();
         if (typeof cordova !== 'undefined' && window.plugins && window.plugins.touchid) {
